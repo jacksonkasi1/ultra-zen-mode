4cdbd129cb162c2340c8ee1075004220fae13631
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-08-04 21:34:44 +0530

    ✨ Add and fix commands for View: Ultra Zen Mode and Toggle Ultra Zen Mode

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,31 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
     let isUltraZenMode = false;
 
-    let disposable = vscode.commands.registerCommand('ultraZenMode.toggle', () => {
+    let viewUltraZenMode = vscode.commands.registerCommand('ultraZenMode.view', () => {
+        // Activate Ultra Zen Mode
+        vscode.commands.executeCommand('workbench.action.toggleZenMode');
+        vscode.commands.executeCommand('workbench.action.toggleFullScreen');
+        vscode.workspace.getConfiguration().update('zenMode.hideTabs', true, vscode.ConfigurationTarget.Global);
+        vscode.workspace.getConfiguration().update('zenMode.hideStatusBar', true, vscode.ConfigurationTarget.Global);
+        vscode.workspace.getConfiguration().update('zenMode.hideActivityBar', true, vscode.ConfigurationTarget.Global);
+        vscode.workspace.getConfiguration().update('zenMode.centerLayout', false, vscode.ConfigurationTarget.Global);
+        vscode.workspace.getConfiguration().update('window.zoomLevel', 0, vscode.ConfigurationTarget.Global);
+        vscode.workspace.getConfiguration().update('editor.minimap.enabled', false, vscode.ConfigurationTarget.Global);
+        vscode.workspace.getConfiguration().update('workbench.editor.enablePreview', false, vscode.ConfigurationTarget.Global);
+        vscode.workspace.getConfiguration().update('workbench.editor.tabCloseButton', 'off', vscode.ConfigurationTarget.Global);
+        vscode.workspace.getConfiguration().update('workbench.activityBar.visible', false, vscode.ConfigurationTarget.Global);
+        vscode.workspace.getConfiguration().update('workbench.statusBar.visible', false, vscode.ConfigurationTarget.Global);
+        vscode.workspace.getConfiguration().update('editor.renderLineHighlight', 'none', vscode.ConfigurationTarget.Global);
+        vscode.workspace.getConfiguration().update('editor.wordWrap', 'on', vscode.ConfigurationTarget.Global);
+        vscode.workspace.getConfiguration().update('editor.scrollBeyondLastLine', false, vscode.ConfigurationTarget.Global);
+        isUltraZenMode = true;
+    });
+
+    let toggleUltraZenMode = vscode.commands.registerCommand('ultraZenMode.toggle', () => {
         if (isUltraZenMode) {
             // Revert to normal mode
             vscode.commands.executeCommand('workbench.action.toggleZenMode');
             vscode.commands.executeCommand('workbench.action.toggleFullScreen');
-            vscode.commands.executeCommand('workbench.action.closeEditorsInGroup');
             isUltraZenMode = false;
         } else {
             // Activate Ultra Zen Mode
@@ -31,7 +50,8 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(viewUltraZenMode);
+    context.subscriptions.push(toggleUltraZenMode);
 }
 
 export function deactivate() {}
